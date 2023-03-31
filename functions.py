76ca7e74208625bf2144ceb1992175f36ccf9b5d
Antonio Vinicius Siqueira <vinicius_seek@yahoo.com>
2023-03-30 21:47:48 -0300

    turns get_puzzle in function to return a tuple containing the word[0] and it's enigmated code[1]

--- functions.py
+++ functions.py
@@ -1,4 +1,8 @@
 def get_puzzle():
+    """
+
+    :return: returns a tuple containing the word[0] and it's enigmated code[1]
+    """
     import random
     bank = ['schutz', 'santa lolla', 'arezzo', 'forever 21', 'charlotte russe']
     word = random.choice(bank)
@@ -10,4 +14,5 @@ def get_puzzle():
         else:
             puzzle += ' '
     puzzle.strip()
-    return puzzle
+
+    return word, puzzle
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import functions
 
 print('\033[1mhangman, the game\n\033[m')
 
-print(functions.get_puzzle())
+quest = functions.get_puzzle()
+
