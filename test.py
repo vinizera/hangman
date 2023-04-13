fa9a75e50f984843800be85b8112f96c77274095
Antonio Vinicius Siqueira <46322262+vinizera@users.noreply.github.com>
2023-04-13 09:51:15 -0300

    test for creation of json file

--- test.py
+++ test.py
@@ -1,10 +1,2 @@
-import random
-
-print('hangman, the game')
-
-words = ['schutz', 'santa lolla', 'arezzo', 'forever 21', 'charlotte russe']
-#words_bet = ['flamengo', 'ponte preta', 'vasco da gama', 'gama', 'nautico', 'corinthians', 'athletico paranaense']
-
-hides = [('_ '*len(i)) for i in words]
-hides = [i.strip() for i in hides]
-
+with open('wordbank.json', 'a+') as bank:
+    json.write('test bank')
